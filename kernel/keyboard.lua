f14a20a142f3ba1b96e75b0be0cc33a7fa089723
Goosey <falconiumpurple@gmail.com>
2025-05-04 03:41:22 +0100

    5

--- kernel/keyboard.lua
+++ kernel/keyboard.lua
@@ -1,20 +1,34 @@
 local keyboard = {}
 
--- This should take in the screen buffer
 keyboard.x = 1
-keyboard.y = 1
+keyboard.y = 2
+local flashOn = false
 local K = 0
+local originalChar = " "
 
 keyboard.update = function(e, code)
-    if K % 4 == 0 then
-        local char = _G.shell.readChar(keyboard.x, keyboard.y)
-        if char then
-            _G.shell.setColour(0x000000, 0xFFFFFF)
-            _G.shell.text(char, false) 
-            _G.clr()
+    local gpu = _G.bootgpu
+    local char = _G.shell.readChar(keyboard.x, keyboard.y) or " "
+
+    if char ~= " " then
+        originalChar = char
+    end
+
+    -- Flash every few ticks
+    if K % 6 == 0 then
+        local fg, bg = 0x000000, 0xFFFFFF
+        if flashOn then
+            fg, bg = 0xFFFFFF, 0x000000 -- Normal colors
         end
+
+        _G.invoke(gpu, "setForeground", fg)
+        _G.invoke(gpu, "setBackground", bg)
+        _G.invoke(gpu, "set", keyboard.x, keyboard.y, originalChar)
+
+        flashOn = not flashOn
         K = 0
     end
+
     K = K + 1
 end
 
